c5edc8f82f4b623d1d9344e8bb7e3485e792955c
beck410 <beck410@hotmail.com>
2014-12-17 15:58:43 -0600

    set up routes for landing page and snapshot page

--- app/js/main.config.js
+++ app/js/main.config.js
@@ -1,5 +1,18 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
+  .config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+      templateUrl: 'views/landing.html'
+    })
+    .when('/snapshot',{
+      templateUrl: 'views/snapshot.html'
+    })
+    .when('/',{
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+  });
 })();
-
