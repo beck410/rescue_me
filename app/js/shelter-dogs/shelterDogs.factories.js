d6aa9152edf5dd051364eba4efc17f3a92d0509c
beck410 <beck410@hotmail.com>
2014-12-18 17:38:14 -0600

    added shelterOrg data to shelterDogs

--- app/js/shelter-dogs/shelterDogs.factories.js
+++ app/js/shelter-dogs/shelterDogs.factories.js
@@ -10,12 +10,32 @@
           var dataToArray = _objectToArray(data);
           _addShelterContactDetails(dataToArray, mainCB,
             function(orgs,dogs,mainCB){
-              mainCB(dogs);
+              var configuredData = _addShelterContactInfo(dogs,orgs);
+              mainCB(configuredData);
             });
         })
         .error(function(err){
           console.log('get shelterDogs error:' + err);
        });
+    }
+
+    //http://plnkr.co/edit/HZzR5ILFq4F7lFu6OlGr
+    function _addShelterContactInfo(dogs, orgs){
+      var newShelterDogArray = [];
+      dogs.forEach(function(dog){
+        var dogId = dog.animalOrgID;
+        var dogShelter = _.filter(orgs,{'orgID':dogId});
+        if(dogShelter.length !== 0){
+          dog.shelterName = dogShelter[0].orgName || '';
+          dog.shelterEmail = dogShelter[0].orgEmail || '';
+          dog.shelterAddress = dogShelter[0].orgAddress || '';
+          dog.shelterCity = dogShelter[0].orgCity || '';
+          dog.shelterPhone = dogShelter[0].orgPhone || '';
+          dog.shelterWebsite = dogShelter[0].orgWebsiteUrl || '';
+          newShelterDogArray.push(dog);
+        }
+      });
+      return newShelterDogArray;
     }
 
     function _objectToArray(data){
