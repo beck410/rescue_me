9fb65631c7dfb6ef5a117c23a95b64055368fa5b
beck410 <beck410@hotmail.com>
2014-12-18 10:38:06 -0600

    added rescue groups url constant

--- app/js/main.constants.js
+++ app/js/main.constants.js
@@ -1,6 +1,7 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
-    .constant('FIREBASE_URL', 'https://bcd-rescue-me.firebaseio.com/');
+    .constant('FIREBASE_URL', 'https://bcd-rescue-me.firebaseio.com/')
+    .constant('RESCUE_GROUPS_URL', 'https://api.rescuegroups.org/http/json/?data=')
 })();
 
