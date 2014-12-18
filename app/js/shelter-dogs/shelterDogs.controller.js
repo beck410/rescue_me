1f07b62c0a994967c7d0d14b7d6639d31e0486d7
beck410 <beck410@hotmail.com>
2014-12-18 11:01:55 -0600

    GET request for shelterDog data on firebase done

--- app/js/shelter-dogs/shelterDogs.controller.js
+++ app/js/shelter-dogs/shelterDogs.controller.js
@@ -3,5 +3,12 @@
   angular.module('rescue_me')
   .controller('shelterDogsController',function(shelterFactory){
 
-  })
+    var vm = this;
+
+    shelterFactory.getShelterDogs(function(data){
+      vm.shelterDogs = data;
+      console.log(vm.shelterDogs);
+    });
+
+  });
 })();
--- app/js/shelter-dogs/shelterDogs.factories.js
+++ app/js/shelter-dogs/shelterDogs.factories.js
@@ -1,7 +1,21 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
-  .factory('shelterFactory',function(){
-    return {};
-  })
+  .factory('shelterFactory',function($http,FIREBASE_URL){
+
+    function getShelterDogs(cb){
+      var url = FIREBASE_URL + 'shelterDogs/.json';
+      $http.get(url)
+        .success(function(data){
+          cb(data);
+        })
+        .error(function(err){
+          console.log('get shelterDogs error:' + err);
+       });
+    }
+
+    return {
+      getShelterDogs: getShelterDogs
+    };
+  });
 })();
