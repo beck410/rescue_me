60bec95d9a2300b20363060b6363f8a9eb48504a
beck410 <beck410@hotmail.com>
2014-12-17 21:55:35 -0600

    added auth controller and factory files

---
+++ app/js/main.controller.js
@@ -0,0 +1,7 @@
+;(function(){
+  'use strict';
+  angular.module('rescue_me')
+  .controller('snapshotController',function(snapshotFactory){
+
+  })
+})();
---
+++ app/js/main.factory.js
@@ -0,0 +1,7 @@
+;(function(){
+  'use strict';
+  angular.module('rescue_me')
+  .factory('snapshotFactory',function(){
+    return {};
+  });
+})();
