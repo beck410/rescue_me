6201357436f01f3ac60b0ce8121569eef6c9369c
beck410 <beck410@hotmail.com>
2014-12-18 10:00:57 -0600

    created basic login functionality

--- app/js/auth/auth.controller.js
+++ app/js/auth/auth.controller.js
@@ -1,7 +1,16 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
-  .controller('loginController', function(authFactory){
+  .controller('loginController', function(authFactory, $scope, $location){
+    var vm = this;
+
+    vm.login= function(){
+      authFactory.login(vm.email, vm.password, function(){
+        // authFactory.getShelterDogs();
+        $location.path('/snapshot');
+        $scope.$apply();
+      });
+    };
 
   })
   .controller('logoutController', function(authFactory){
@@ -12,5 +21,5 @@
   })
   .controller('changePasswordController', function(authFactory){
 
-  })
+  });
 })();
--- app/js/auth/auth.factories.js
+++ app/js/auth/auth.factories.js
@@ -1,7 +1,26 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
-  .factory('authFactory', function(){
-    return {};
-  })
+  .factory('authFactory', function($http, FIREBASE_URL, $location){
+
+    function _login(email,password,cb){
+      var ref= new Firebase(FIREBASE_URL);
+      console.log(ref);
+      ref.authWithPassword({
+        email: email,
+        password: password,
+      }, function(error, authData){
+        if(error === null) {
+          console.log('user logged in', authData);
+          cb();
+        } else {
+          console.log('Error creating user:' + error);
+        }
+      });
+    }
+
+    return {
+      login: _login
+    };
+  });
 })();
