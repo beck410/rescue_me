ba104216d96c706dd9c40b5820372aae811910a2
beck410 <beck410@hotmail.com>
2014-12-28 17:19:39 -0600

    GET request for potential dogs and callback successful

--- app/js/potential-dogs/potentialDogs.controller.js
+++ app/js/potential-dogs/potentialDogs.controller.js
@@ -2,6 +2,10 @@
   'use strict';
   angular.module('rescue_me')
   .controller('potentialDogsController',function(potentialFactory){
-
-  })
+    var vm = this;
+    potentialFactory.getPotentialDogs(function(dogs){
+      vm.potentialDogs = dogs;
+      console.log(vm.potentialDogs);
+    });
+  });
 })();
--- app/js/potential-dogs/potentialDogs.factories.js
+++ app/js/potential-dogs/potentialDogs.factories.js
@@ -1,8 +1,22 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
-  .factory('potentialFactory',function(){
-    return {};
+  .factory('potentialFactory',function(FIREBASE_URL, $http){
+    function getPotentialDogs(cb){
+      var url = FIREBASE_URL + 'potentialDogs.json';
+      $http.get(url)
+      .success(function(dogs){
+        console.log('potential dogs pulled from fb');
+        cb(dogs);
+      })
+      .error(function(err){
+        console.log('potential dogs list error: ' + err);
+      });
+    }
 
-  })
+    return {
+      getPotentialDogs: getPotentialDogs
+    };
+
+  });
 })();
