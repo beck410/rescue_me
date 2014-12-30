802aeceb111b78e08485d69d9136251721d98f63
beck410 <beck410@hotmail.com>
2014-12-30 09:11:11 -0600

    GET request for rescue dogs list done and callback successful

--- app/js/rescue-dogs/rescueDogs.controller.js
+++ app/js/rescue-dogs/rescueDogs.controller.js
@@ -2,6 +2,10 @@
   'use strict';
   angular.module('rescue_me')
   .controller('rescueDogsController',function(rescueFactory){
-
-  })
+    var vm = this;
+    rescueFactory.getRescueDogs(function(dogs){
+      vm.rescueDogs = dogs;
+      console.log(vm.rescueDogs);
+    });
+  });
 })();
--- app/js/rescue-dogs/rescueDogs.factories.js
+++ app/js/rescue-dogs/rescueDogs.factories.js
@@ -1,7 +1,20 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
-  .factory('rescueFactory',function(){
-    return {};
-  })
+  .factory('rescueFactory',function(FIREBASE_URL,$http){
+
+    function getRescueDogs(cb){
+      var url = FIREBASE_URL + 'rescueDogs.json'
+      $http.get(url)
+      .success(function(dogs){
+        cb(dogs);
+      })
+      .error(function(){
+        console.log('rescue dog list error: ' + err);
+      });
+    }
+    return {
+      getRescueDogs: getRescueDogs
+    };
+  });
 })();
