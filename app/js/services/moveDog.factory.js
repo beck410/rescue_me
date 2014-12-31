ba13f59426640d71b74595060a53f988e570535d
beck410 <beck410@hotmail.com>
2014-12-31 16:07:47 -0600

    changed addToNextList to delete from original List and reroute to dog-details-form

--- app/js/services/moveDog.factory.js
+++ app/js/services/moveDog.factory.js
@@ -4,29 +4,30 @@
   .factory('moveDogFactory',function(FIREBASE_URL,$http){
 
     function addToNextList(newDogDB,oldDogDB,dogID,dog,cb){
-      _postDog(newDogDB,dog,cb);
-      _deleteDog(oldDogDB,dogID);
+      // _postDog(newDogDB,dog,cb);
+      _deleteDog(oldDogDB,dogID,dog,cb);
     }
 
-    function _postDog(newDogDB,dog,cb){
-      var url = FIREBASE_URL + newDogDB + '.json';
-      $http.post(url,dog)
-        .success(function(dog){
-          if(cb){
-            cb(dog);
-          }
-        })
-        .error(function(err){
-          console.log('post dog error: ' + err);
-        });
-    }
+    // function _postDog(newDogDB,dog,cb){
+    //   var url = FIREBASE_URL + newDogDB + '.json';
+    //   $http.post(url,dog)
+    //     .success(function(dog){
+    //       if(cb){
+    //         cb(dog);
+    //       }
+    //     })
+    //     .error(function(err){
+    //       console.log('post dog error: ' + err);
+    //     });
+    // }
 
-    function _deleteDog(oldDogDB,dogID){
+    function _deleteDog(oldDogDB,dogID,dog,cb){
       var url = FIREBASE_URL + oldDogDB  + '/' + dogID + '.json';
       console.log(url)
       $http.delete(url)
       .success(function(){
         console.log('deleted')
+        cb(dog);
       })
       .error(function(err){
         console.log('delete dog error: ' + err);
