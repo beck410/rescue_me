a823ea85ed2f5cc30be3a5491c7a24111f8170b4
beck410 <beck410@hotmail.com>
2014-12-30 19:08:18 -0600

    added removeDog function to removeDog factory

---
+++ app/js/services/removeDog.factory.js
@@ -0,0 +1,21 @@
+;(function(){
+  'use strict';
+  angular.module('rescue_me')
+  .factory('removeDogFactory',function(){
+
+    function removeDog(dogDB,dog,cb){
+      var url = FIREBASE_URL + 'rescueDogs/' + dog + '.json';
+      $http.delete(url)
+      .success(function(){
+        cb(dog);
+      })
+      .error(function(err){
+        console.log('delete rescue dog error: ' + err);
+      });
+    }
+
+    return {
+      removeDog: removeDog
+    };
+  });
+})();
