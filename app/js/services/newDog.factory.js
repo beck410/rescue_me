d40b828c1f6924e445144abeb8099565a8192ca1
beck410 <beck410@hotmail.com>
2014-12-31 23:41:49 -0600

    added addNewDog factory

---
+++ app/js/services/newDog.factory.js
@@ -0,0 +1,13 @@
+;(function(){
+  'use strict';
+  angular.module('rescue_me')
+  .factory('addNewDogFactory',function(){
+    function addDog(){
+
+    }
+
+    return {
+      addDog: addDog
+    };
+  });
+})();
