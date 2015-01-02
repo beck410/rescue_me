9adb901f63107c78af5d666caaf2290a11400b34
beck410 <beck410@hotmail.com>
2015-01-01 21:30:43 -0600

    rerouted register page after rescueDetails added to db

--- app/js/services/register.factory.js
+++ app/js/services/register.factory.js
@@ -1,32 +1,36 @@
 ;(function(){
   'use strict';
   angular.module('rescue_me')
-  .factory('registerFactory',function(FIREBASE_URL,$http){
+  .factory('registerFactory',function(loginFactory,requestURL,FIREBASE_URL,$http){
     var ref = new Firebase(FIREBASE_URL);
 
-    function register(user, cb){
+    function register(user,cb){
       ref.createUser({
         email: user.email,
         password: user.password
-      },function(error, authData){
+      },function(error,authData){
         if(error === null){
           console.log('user created successfully', authData);
-          _addRescueDetails(user,cb);
-        } else {
+          loginFactory.login(user.email, user.password, function(){
+            _addRescueDetails(user,authData.uid,cb);
+          });
+        } else{
           console.log('Error creating user: ' + error);
         }
       });
     }
 
-    function _addRescueDetails(rescue,cb){
-      var url = FIREBASE_URL + 'rescueDetails.json';
+    function _addRescueDetails(rescue,id,cb){
+      var url = requestURL.url('rescueDetails');
+      console.log(url);
       $http.put(url,rescue)
-    .success(function(){
-      console.log('Rescue details added');
-    })
+      .success(function(){
+        console.log('Rescue details added');
+        cb();
+      })
       .error(function(err){
         console.log('adding rescue details error: ' + err);
-      })
+      });
     }
 
     return {
