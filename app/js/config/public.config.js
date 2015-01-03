efae0939f83fec6daa786cfcc9b91ab14892529b
beck410 <beck410@hotmail.com>
2015-01-03 14:12:50 -0600

    added run on config to change header for private pages

--- app/js/config/public.config.js
+++ app/js/config/public.config.js
@@ -19,7 +19,16 @@
   .run(function($rootScope,checkAccessFactory){
     $rootScope.$on('$routeChangeStart', function(event,nextRoute){
       if(nextRoute.$$route && nextRoute.$$route.needLogin){
+        $('.main-header').css('background','url("../images/header-img-2.png")');
+        $('.main-header').css('height','200px');
+        $('.nav-main').css('padding-top','160px');
+        $('.nav-main').css('padding-left','40px');
         checkAccessFactory.requireLogin();
+      } else {
+         $('.main-header').css('background','url("../images/header-img.png")');
+        $('.main-header').css('height','550px');
+        $('.nav-main').css('padding-top','200px');
+        $('.nav-main').css('padding-left','55px');
       }
     });
   });
