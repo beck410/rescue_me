c83ab60bf65c034042e37ec6f30756b91f53f1fa
beck410 <beck410@hotmail.com>
2015-01-07 12:23:32 -0600

    added slideshow functionality to rescue dogs

--- app/js/controllers/rescueDogs.controller.js
+++ app/js/controllers/rescueDogs.controller.js
@@ -22,70 +22,97 @@
       $location.path('/rescue-dogs/add');
     };
   })
-  .controller('showRescueController', function(dogDetailsFactory, $routeParams,$location,completeDogDetails){
-    var vm = this;
-    var id = $routeParams.id;
-    vm.ownNotes = true;
-    vm.foster = true;
+.controller('showRescueController', function(dogDetailsFactory, $routeParams,$location,completeDogDetails,slideshowFactory){
+  var vm = this;
+  var id = $routeParams.id;
+  vm.ownNotes = true;
+  vm.foster = true;
 
-    dogDetailsFactory.getDogDetails('rescueDogs',id,function(rescueDog){
-      var completeRescueDog = completeDogDetails.fillEmptyDetails(rescueDog)
-      console.log(completeRescueDog)
-      vm.dog = completeRescueDog;
+  dogDetailsFactory.getDogDetails('rescueDogs',id,function(rescueDog){
+    var completeRescueDog = completeDogDetails.fillEmptyDetails(rescueDog)
+    console.log(completeRescueDog)
+    vm.dog = completeRescueDog;
+    dogDetailsFactory.getFullSizeImages(vm.dog.animalPictures,function(images){
+      vm.dog.fullSizeImages = images;
     });
 
-    vm.editDetailsDetails = function(){
-     $location.path('/rescue-dogs/' + id + '/edit');
-    };
-  })
-  .controller('moveRescueDog',function(moveDogFactory, dogDetailsFactory,$routeParams,$location,rescuedDogsCounter){
-    var vm = this;
-    var id = $routeParams.id;
-    vm.dogGroup = 'potential-dogs';
-    vm.rescueDog = true;
+    dogDetailsFactory.getThumbnailImages(vm.dog.animalPictures,function(images){
+      vm.dog.thumbnails = images;
+      vm.imgStartIndex = 3;
+      vm.imgEndIndex = 3;
+      vm.thumbnailLength = _.size(images);
 
-    dogDetailsFactory.getDogDetails('potentialDogs',id,function(dogDetails){
-      vm.dog = dogDetails;
+      vm.prevImgButton = function(){
+        return slideshowFactory.prevDogButton(vm.imgEndIndex, 3);
+      }
+
+      vm.prevImg = function(){
+        vm.imgEndIndex -= 3;
+      }
+
+      vm.nextImgButton = function(){
+        return slideshowFactory.nextDogButton(vm.imgEndIndex,vm.thumbnailLength);
+      }
+
+      vm.nextImg = function(){
+        vm.imgEndIndex += 3;
+      };
     });
 
-    vm.submitDogDetails = function(){
-      moveDogFactory.addDogToList('rescueDogs','potentialDogs',id,vm.dog,function(){
-        rescuedDogsCounter.updateCounter();
-        $location.path('/rescue-dogs/');
-      });
-    };
-  })
-  .controller('editRescueDog',function(editDetailsFactory,dogDetailsFactory,$routeParams,$location){
-    var vm = this;
-    var id = $routeParams.id;
-    vm.dogGroup = 'rescue-dogs';
-    vm.rescueDog = true;
-
-    dogDetailsFactory.getDogDetails('rescueDogs',id,function(dogDetails){
-      vm.dog = dogDetails;
-    });
-
-    vm.submitDogDetails = function(){
-      editDetailsFactory.editDetails('rescueDogs',id,vm.dog,function(){
-        rescuedDogsCounter.updateCounter();
-        $location.path('rescue-dogs/');
-      });
-    };
-
-  })
-  .controller('addRescueDog',function(rescuedDogsCounter,addNewDogFactory,$location){
-    var vm = this;
-    vm.dogGroup = 'rescue-dogs';
-    vm.rescueDog = true;
-
-    vm.submitDogDetails = function(){
-      addNewDogFactory.addDog(vm.dog,'rescueDogs',function(dog){
-        rescuedDogsCounter.updateCounter();
-        vm.dogs = vm.dogs || {};
-        vm.dogs[dog.name] = vm.dog;
-        vm.dog = {};
-        $location.path('/rescue-dogs');
-      });
-    };
   });
+
+  vm.editDetailsDetails = function(){
+    $location.path('/rescue-dogs/' + id + '/edit');
+  };
+})
+.controller('moveRescueDog',function(moveDogFactory, dogDetailsFactory,$routeParams,$location,rescuedDogsCounter){
+  var vm = this;
+  var id = $routeParams.id;
+  vm.dogGroup = 'potential-dogs';
+  vm.rescueDog = true;
+
+  dogDetailsFactory.getDogDetails('potentialDogs',id,function(dogDetails){
+    vm.dog = dogDetails;
+  });
+
+  vm.submitDogDetails = function(){
+    moveDogFactory.addDogToList('rescueDogs','potentialDogs',id,vm.dog,function(){
+      rescuedDogsCounter.updateCounter();
+      $location.path('/rescue-dogs/');
+    });
+  };
+})
+.controller('editRescueDog',function(editDetailsFactory,dogDetailsFactory,$routeParams,$location){
+  var vm = this;
+  var id = $routeParams.id;
+  vm.dogGroup = 'rescue-dogs';
+  vm.rescueDog = true;
+
+  dogDetailsFactory.getDogDetails('rescueDogs',id,function(dogDetails){
+    vm.dog = dogDetails;
+  });
+
+  vm.submitDogDetails = function(){
+    editDetailsFactory.editDetails('rescueDogs',id,vm.dog,function(){
+      rescuedDogsCounter.updateCounter();
+      $location.path('rescue-dogs/');
+    });
+  };
+
+})
+.controller('addRescueDog',function(rescuedDogsCounter,addNewDogFactory,$location){
+  var vm = this;
+  vm.dogGroup = 'rescue-dogs';
+  vm.rescueDog = true;
+
+  vm.submitDogDetails = function(){
+    addNewDogFactory.addDog(vm.dog,'rescueDogs',function(dog){
+      rescuedDogsCounter.updateCounter();
+      vm.dogs = vm.dogs || {};
+      vm.dogs[dog.name] = vm.dog;
+      vm.dog = {};
+      $location.path('/rescue-dogs');
+    });
+  };
+});
 })();
