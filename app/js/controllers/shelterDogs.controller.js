e0e2e90a1340d7055f135f3bb0a54fa1e0805558
beck410 <beck410@hotmail.com>
2015-01-07 11:43:52 -0600

    sanitized animalDescription before inserting in dog-details

--- app/js/controllers/shelterDogs.controller.js
+++ app/js/controllers/shelterDogs.controller.js
@@ -16,7 +16,7 @@
     });
 
     vm.addToNextList = function(shelterID){
-        $location.path('/potential-dogs/' + shelterID + '/move');
+      $location.path('/potential-dogs/' + shelterID + '/move');
     };
 
     vm.filteredShelterDogs = function(){
@@ -35,27 +35,48 @@
 
     vm.clearForm = function(){
       console.log('working')
-      console.log(vm.apiKeys);
+        console.log(vm.apiKeys);
       vm.apiKeys = {};
     }
   })
-  .controller('showShelterController', function(dogDetailsFactory,$routeParams,completeDogDetails,$sce,$sanitize){
-    var vm = this;
-    var id = $routeParams.id;
-    vm.shelter = true;
+.controller('showShelterController', function(dogDetailsFactory,$routeParams,completeDogDetails,$sce,$sanitize,slideshowFactory){
+  var vm = this;
+  var id = $routeParams.id;
+  vm.shelter = true;
 
-    dogDetailsFactory.getDogDetails('shelterDogs',id,function(shelterDog){
-      var completeShelterDog = completeDogDetails.fillEmptyDetails(shelterDog);
-      vm.dog = completeShelterDog;
-      dogDetailsFactory.getFullSizeImages(vm.dog.animalPictures,function(images){
-        vm.dog.fullSizeImages = images;
-        console.log(vm.dog.fullSizeImages)
-      });
-      dogDetailsFactory.getThumbnailImages(vm.dog.animalPictures,function(images){
-        vm.dog.thumbnails = images;
-        console.log(vm.dog.thumbnails);
-      });
-      vm.dog.sanitizedAnimalDescription = $sanitize(vm.dog.animalDescription);
+  dogDetailsFactory.getDogDetails('shelterDogs',id,function(shelterDog){
+    var completeShelterDog = completeDogDetails.fillEmptyDetails(shelterDog);
+    vm.dog = completeShelterDog;
+    dogDetailsFactory.getFullSizeImages(vm.dog.animalPictures,function(images){
+      vm.dog.fullSizeImages = images;
     });
+    dogDetailsFactory.getThumbnailImages(vm.dog.animalPictures,function(images){
+      vm.dog.thumbnails = images;
+      vm.imgStartIndex = 3;
+      vm.imgEndIndex = 3;
+      vm.thumbnailLength = _.size(images);
+
+      vm.prevImgButton = function(){
+        return slideshowFactory.prevDogButton(vm.imgEndIndex, 3);
+      }
+
+      vm.prevImg = function(){
+        console.log('called')
+        vm.imgEndIndex -= 3;
+      }
+
+      vm.nextImgButton = function(){
+        return slideshowFactory.nextDogButton(vm.imgEndIndex,vm.thumbnailLength);
+      }
+
+      vm.nextImg = function(){
+        console.log('called')
+        vm.imgEndIndex += 3;
+      }
+
+
+    });
+    vm.dog.sanitizedAnimalDescription = $sanitize(vm.dog.animalDescription);
   });
+});
 })();
