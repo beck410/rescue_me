bf2863d80fc5036659fca47c282e042244f57006
beck410 <beck410@hotmail.com>
2015-01-11 14:50:36 -0600

    added bootstrap ui as a dependency

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 ;(function(){
   'use strict';
-  angular.module('rescue_me',['ngRoute','angular-loading-bar','ngSanitize','angularFileUpload'])
+  angular.module('rescue_me',['ngRoute','angular-loading-bar','ngSanitize','angularFileUpload','ui.bootstrap'])
 })();
 
