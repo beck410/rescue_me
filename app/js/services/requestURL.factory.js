f2f9f89a51bb3dd2dd4928360f5e621e95220645
beck410 <beck410@hotmail.com>
2015-01-17 19:24:43 -0600

    snapshot info now available publicly

--- app/js/services/requestURL.factory.js
+++ app/js/services/requestURL.factory.js
@@ -3,11 +3,19 @@
   angular.module('rescue_me')
   .factory('requestURL',function(FIREBASE_URL,$rootScope){
     function url(db,rescueName,id){
-      if(id){
-        return FIREBASE_URL + 'rescueOrgs/' + rescueName + '/' + db + '/' + id + '.json?auth=' + $rootScope.user.token;
-      } else if($rootScope.user){
-        return FIREBASE_URL + 'rescueOrgs/' + rescueName + '/' + db + '.json?auth=' + $rootScope.user.token;
-      }     
+      if($rootScope.user){
+        if(id){
+          return FIREBASE_URL + 'rescueOrgs/' + rescueName + '/' + db + '/' + id + '.json?auth=' + $rootScope.user.token;
+        } else{
+          return FIREBASE_URL + 'rescueOrgs/' + rescueName + '/' + db + '.json?auth=' + $rootScope.user.token;
+        }    
+      } else{
+        if(id){
+          return FIREBASE_URL + 'rescueOrgs/' + rescueName + '/' + db + '/' + id + '.json';
+        } else{
+          return FIREBASE_URL + 'rescueOrgs/' + rescueName + '/' + db + '.json';
+        }    
+      }
     }
     return {
       url: url
