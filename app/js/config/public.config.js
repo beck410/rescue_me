4681e5155a074c9d4eef0116027c34e5022331f3
beck410 <beck410@hotmail.com>
2015-01-18 14:35:13 -0600

    fixed header config for public routes based on logged in

--- app/js/config/public.config.js
+++ app/js/config/public.config.js
@@ -24,12 +24,14 @@
   })
   .run(function($rootScope,checkAccessFactory){
     $rootScope.$on('$routeChangeStart', function(event,nextRoute){
-      if(nextRoute.$$route && nextRoute.$$route.needLogin || nextRoute.$$route && nextRoute.$$route.smallHeader){
+      if(nextRoute.$$route && nextRoute.$$route.needLogin){
+        checkAccessFactory.requireLogin();
+      }
+      if($rootScope.user){
         $('.main-header').css('background','url("../images/header-img-2.png")');
         $('.main-header').css('height','200px');
         $('.nav-main').css('padding-top','160px');
         $('.nav-main').css('padding-left','40px');
-        checkAccessFactory.requireLogin();
       } else {
         $('.main-header').css('background','url("../images/header-img.png")');
         $('.main-header').css('height','550px');
